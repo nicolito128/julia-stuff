cc0c0fce72b0de7dea6e16e49fed31202be27c4e
nicolito128 <nicolito128@hotmail.com>
2024-04-13 17:33:40 -0300

    update: find_root

--- learn/find_root.jl
+++ learn/find_root.jl
@@ -1,6 +1,16 @@
+"""
+    # Find the root(index) of a number using binary search
+    - Ref: https://en.wikipedia.org/wiki/Binary_search_algorithm
+    - Ref: https://www.geeksforgeeks.org/binary-search/
 
-# Find the root(index) of a number using binary search
-function find_root(n; index=2, epsilon=0.01)
+    # Example
+    - find_root(4) ~= √4
+    - find_root(7, index=3) ~= ∛7
+    - find_root(8, epsilon=0.001, digits=4) ~= √8
+    -- Shows the first 4 digits of √8 and has a tolerance of 0.001 (epsilon)
+"""
+
+function find_root(n; index=2, epsilon=0.01, digits = 3)
     low = 0
     hight = n
     guess = (hight + low) / 2
@@ -20,10 +30,10 @@ function find_root(n; index=2, epsilon=0.01)
     end
 
     println("\tGuess = $guess\tGuess for (index=$index) = $(guess^index)")
-    guess = round(guess, digits = 3)
+    guess = round(guess, digits = digits)
     println("\nThe root($index) of $n is $guess")
 end
 
 find_root(7, index=3)
-find_root(8)
+find_root(8, epsilon=(1 / (10^10)), digits = 10)
 find_root(4)
