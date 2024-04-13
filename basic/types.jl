f04086ee2f91320e9ce0ff80ae62287971e398ab
nicolito128 <nicolito128@hotmail.com>
2024-04-13 17:05:15 -0300

    update: types

--- basic/types.jl
+++ basic/types.jl
@@ -1,18 +1,40 @@
-# Data type
+"""
+    # Data type
+    - Ref: https://docs.julialang.org/en/v1/manual/types/
+"""
 
+# Type declaration
+x::Int8 = 100
+y::Float64 = 3.14159
+c::Char = 'c'
+f::Bool = (-10) < (-20)
+
+# Numbers
+# Int64
 typeof(1)
+# Float64
 typeof(1.0)
+# UInt8
 typeof(0x01)
+# Float32
 typeof(0.3f0)
-
-typeof('a')
-typeof("collection of chars")
-typeof([1, 2, 3])
-typeof(('a', 'b', Int('c')))
-
-π
+# Irrational{:\pi}
 typeof(π)
-
+# Complex{Int64}
 typeof(1 + 2im)
 
+# Collections
+# Char
+typeof('a')
+# String
+typeof("collection of chars")
+# Vector{Int64}
+typeof([1, 2, 3])
+# Vector{Any}
+typeof([0, 2.3, 'x'])
+# Tuple{Char, Bool, Int64}
+typeof(('a', true, Int64('c')))
+
+# Symbol
 typeof(:ok)
+typeof(:custom_symbol)
